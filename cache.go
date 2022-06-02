ecb3e258c486a6db25a87d8ffd4664a0e7b79ac1
Andrey Semenov <semenowav@gmail.com>
2022-06-02 14:05:13 +0300

    first attempt

--- cache.go
+++ cache.go
@@ -1,23 +1,63 @@
 package cache
 
-import "time"
+import (
+	"time"
+	"fmt"
+)
 
 type Cache struct {
+	storage, timer map[string]string
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{
+		storage: make(map[string]string),
+		timer: make(map[string]string),
+	}
 }
 
-func (receiver) Get(key string) (string, bool) {
+func (cache Cache) Get(key string) (string, bool) {
 
+	cache.CheckTimer()
+
+	value, ok := cache.storage[key]
+	return value, ok
 }
 
-func (receiver) Put(key, value string) {
+func (cache Cache) Put(key, value string) {
+	cache.storage[key] = value
 }
 
-func (receiver) Keys() []string {
+func (cache Cache) Keys() []string {
+
+	cache.CheckTimer()
+
+	keys := []string{}
+    for key := range cache.storage {
+        keys = append(keys, key)
+    }
+
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (cache Cache) PutTill(key, value string, deadline time.Time) {
+	cache.storage[key] = value
+	cache.timer[key] = deadline.String()
+}
+
+func (cache Cache) CheckTimer() {
+	for key := range cache.storage {
+		if timeString, ok := cache.timer[key]; ok {
+			dueDate, error := time.Parse("dd-mm-yyyy HH:mm:ss", timeString)
+			if error != nil {
+				fmt.Println(error)
+				return
+			}
+
+			if dueDate.Before(time.Now()) {
+				delete(cache.storage, key)
+				delete(cache.timer, key)
+			}
+		}
+	}
 }
